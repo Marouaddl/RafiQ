324318a32277515d589a1ece8179c60051de53f1
maroua <marouadjili2@gmail.com>
2025-10-14 11:39:33 +0100

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-blue-600">Tailwind CSS fonctionne 🎉</h1>
+    </>
+  )
+}
+
+export default App
